d5b363084f9e2e5c3d63336690a7089c5255e5c6
AadityaUNI <aadi@DESKTOP-K2FC5Q2.localdomain>
2026-07-30 21:12:46 +0530

    added cors

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:8080"],  # Vite dev server
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:8080", "http://206.189.43.151:8080"],  # Vite dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
